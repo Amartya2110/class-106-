6d56e4cfe167f851ffebba6b8dc71140e4ad2cbe
Amartya2110 <87061240+Amartya2110@users.noreply.github.com>
2023-04-20 19:33:33 +0800

    Add files via upload

---
+++ project106.py
@@ -0,0 +1,24 @@
+import csv 
+import numpy as np 
+import plotly.express as px
+
+def plotFigure(data_path):
+    with open(data_path) as csv.file:
+        df = csv.DictReader(csv_file)
+        fig = px.scatter(df,x="Days Present", y="Marks In Percentage")
+        fig.show()
+
+def findCorrelation(datasource):
+    correlation = np.corrcoef(datasource["x"], datasource["y"])
+    print("Correlation between Marks in percentage and Days present :- \n--->", correlation[0,1])
+
+def getDataSource(dath_path):
+    marks_in_percentage = []
+    days_present = []
+    with open(data_path) as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            marks_in_percentage.append(float(row["Marks In Percentage"]))
+            days_present.append(float(row["Days Present"]))
+
+        return {"x": marks_in_percentage, "y": days_present}
